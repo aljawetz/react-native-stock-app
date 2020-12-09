c3d2d5d2ae33bee23e2c8864d55b0ea76509f23b
Arthur Jawetz <63313553+aljawetz@users.noreply.github.com>
2020-12-09 17:36:24 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyD1xIYTzokX0aD3hNDQcSnO2y8BUjEjVGI",
-  authDomain: "artistfy-a8fbe.firebaseapp.com",
-  databaseURL: "https://artistfy-a8fbe.firebaseio.com",
-  projectId: "artistfy-a8fbe",
-  storageBucket: "artistfy-a8fbe.appspot.com",
-  messagingSenderId: "689926227838",
-  appId: "1:689926227838:web:060bc1c9f618f0245d952f",
-  measurementId: "G-HPEVVX5KW3"
+  apiKey: "########",
+  authDomain: "########",
+  databaseURL: "########",
+  projectId: "########",
+  storageBucket: "########",
+  messagingSenderId: "########",
+  appId: "########",
+  measurementId: "########"
 };
 
-export const apiKey = 'C77RP7AVYDQ3KWVC';
+export const apiKey = '########';
